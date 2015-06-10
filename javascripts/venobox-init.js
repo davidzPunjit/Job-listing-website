a34b48cb89122d2527851bb13d66013c8e70d9f3
David Zhang <david8zhang@berkeley.edu>
2015-06-10 16:02:03 -0700

    first commit

---
+++ javascripts/venobox-init.js
@@ -0,0 +1,21 @@
+ //Venobox Responsive Lightbox
+   
+ (function(){
+  "use strict";
+
+
+$(function ($) {
+
+
+   $('.venobox, .popup-image, .popup-youtube, .popup-vimeo, .popup-gallery, .popup-dailymotion').venobox({
+    numeratio: true
+    }); 
+
+
+
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
